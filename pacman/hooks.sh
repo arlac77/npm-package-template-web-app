d5e0caed25f6d80747cad1ccc02b324895247205
Markus Felten <Markus.Felten@gmx.de>
2020-06-17 02:08:14 +0200

    feat: compress content after installation

---
+++ pacman/hooks.sh
@@ -0,0 +1,22 @@
+
+post_install() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map)
+	systemctl reload nginx
+}
+
+pre_upgrade() {
+	(cd {{installdir}}; rm *.gz)
+}
+
+post_upgrade() {
+	(cd {{installdir}}; gzip -k -9 *.html *.css *.mjs *.json *.map)
+	systemctl reload nginx
+}
+
+pre_remove() {
+	(cd {{installdir}}; rm *.gz)
+}
+
+post_remove() {
+	systemctl reload nginx
+}
